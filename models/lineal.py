2ef92a6c2b7ee124f758e51a5508a472b10701a7
TeoEchavarria <echavarria.0042@gmail.com>
2023-07-30 22:30:49 -0500

    Lineal Simple Model Created

---
+++ models/lineal.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+class Lineal_Simpledeac_Model:
+
+    @classmethod
+    def prediction(dataset):
+        X = dataset.iloc[:, :-1].values
+        y = dataset.iloc[:, 1].values
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+        regression = LinearRegression()
+        regression.fit(X_train, y_train)
+
+        y_pred = regression.predict(X_test)
+
+        #EVALUACION DE % DE ERROR
+        error_aprox = np.mean([ abs((y_pred[i]-y_test[i])/y_test[i]) for i in range(len(X_test))])
+
+        return {"error" : error_aprox, "prediction" : y_pred}
