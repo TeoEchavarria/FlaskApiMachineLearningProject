813923d3667f7cbccbd64bfc1b50503b69d9c6fe
TeoEchavarria <echavarria.0042@gmail.com>
2023-07-31 14:57:04 -0500

    Update simple_regressions_controller.py

--- controllers/simple_regressions_controller.py
+++ controllers/simple_regressions_controller.py
@@ -5,12 +5,12 @@ simple_regressions_route = Blueprint("simple_regressions", __name__)
 
 @simple_regressions_route.route("/lineal", methods=["GET"])
 def get_lineal_simple_model():
-    data = request.get_json()
+    data = request.get_json(force = True)
     return make_response(data)
     #return SimpleRegressions.lineal(data)
 
 @simple_regressions_route.route("/polynomial", methods=["GET"])
 def get_polynomial_simple_model():
-    data = request.get_json()
+    data = request.get_json(force = True)
     return make_response(data)
     #return SimpleRegressions.polynomial(data)
