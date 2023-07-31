1c3d0572a62caddc906aa04382da538b3eb4ec07
TeoEchavarria <echavarria.0042@gmail.com>
2023-07-31 14:45:06 -0500

    Update simple_regressions_controller.py

--- controllers/simple_regressions_controller.py
+++ controllers/simple_regressions_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, make_response
 from services.simple_regressions import SimpleRegressions
 
 simple_regressions_route = Blueprint("simple_regressions", __name__)
@@ -6,10 +6,11 @@ simple_regressions_route = Blueprint("simple_regressions", __name__)
 @simple_regressions_route.route("/lineal", methods=["GET"])
 def get_lineal_simple_model():
     data = request.get_json()
-    print(data, type(data))
-    return SimpleRegressions.lineal(data)
+    return make_response(data)
+    #return SimpleRegressions.lineal(data)
 
 @simple_regressions_route.route("/polynomial", methods=["GET"])
 def get_polynomial_simple_model():
     data = request.get_json()
-    return SimpleRegressions.polynomial(data)
+    return make_response(data)
+    #return SimpleRegressions.polynomial(data)
