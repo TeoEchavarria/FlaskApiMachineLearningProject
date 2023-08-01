3c690957a7ff28cdba4feb5e32e4f7867d557b9d
TeoEchavarria <echavarria.0042@gmail.com>
2023-07-31 20:03:58 -0500

    Change Methods

--- controllers/simple_regressions_controller.py
+++ controllers/simple_regressions_controller.py
@@ -3,14 +3,14 @@ from services.simple_regressions import SimpleRegressions
 
 simple_regressions_route = Blueprint("simple_regressions", __name__)
 
-@simple_regressions_route.route("/lineal", methods=["GET"])
+@simple_regressions_route.route("/lineal", methods=["POST"])
 def get_lineal_simple_model():
     data = request.get_json()
     print(data)
     #return make_response({"message" : "lineal"})
     return SimpleRegressions.lineal(data)
 
-@simple_regressions_route.route("/polynomial", methods=["GET"])
+@simple_regressions_route.route("/polynomial", methods=["POST"])
 def get_polynomial_simple_model():
     data = request.get_json()
     print(data)
