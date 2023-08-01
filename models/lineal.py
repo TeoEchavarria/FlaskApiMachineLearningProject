20e4d87183dad3efd2ef8fdb2b109507462c9914
TeoEchavarria <echavarria.0042@gmail.com>
2023-07-31 22:40:29 -0500

    Chage Range Pred

--- models/lineal.py
+++ models/lineal.py
@@ -16,7 +16,7 @@ class LinealSimpleModel:
         regression = LinearRegression()
         regression.fit(X_train, y_train)
 
-        y_pred = regression.predict(X_train)
+        y_pred = regression.predict(X)
 
         #EVALUACION DE % DE ERROR
         error_aprox = np.mean([ abs((y_pred[i]-y_test[i])/y_test[i]) for i in range(len(X_test))])
