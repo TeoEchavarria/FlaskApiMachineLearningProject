6eb7854a5b2dfd1f3194c549cfbe85691fb63465
TeoEchavarria <echavarria.0042@gmail.com>
2023-08-03 14:04:31 -0500

    Update Simple Regression Return

--- models/lineal.py
+++ models/lineal.py
@@ -21,4 +21,4 @@ class LinealSimpleModel:
         #EVALUACION DE % DE ERROR
         error_aprox = np.mean([ abs((y_pred[i]-y_test[i])/y_test[i]) for i in range(len(X_test))])
 
-        return {"error" : error_aprox, "prediction" : list(y_pred)}
+        return {"error" : error_aprox, "prediction" : list(y_pred), "X_grid" : [i[0] for i in X.tolist()]}
