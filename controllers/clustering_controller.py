1e276661ac9e2f1489367d1aa79222606adbc4f5
TeoEchavarria <echavarria.0042@gmail.com>
2023-08-13 14:36:58 -0500

    Update Clustering Controllers

--- controllers/clustering_controller.py
+++ controllers/clustering_controller.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request, make_response
-from services
+from services.clustering import Clustering
 
 clustering = Blueprint("clustering", __name__)
 
 @clustering.route("/nearest_neighbors", methods=["POST"])
 def get_nearest_neighbors():
     data = request.get_json()
-    #return SimpleRegressions.lineal(data)
+    return Clustering.nearestNeighbors(data)
 
 @clustering.route("/logistics", methods=["POST"])
 def get_logistics():
     data = request.get_json()
-    #return SimpleRegressions.polynomial(data)
+    return Clustering.logistics(data)
